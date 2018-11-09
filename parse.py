099b3dab35a96aff17e7d5c64172b8adc9b71e3a
Stefan Balke <stefan@balke.at>
2018-11-09 15:54:47 +0100

    enhance json output

--- parse.py
+++ parse.py
@@ -2,6 +2,39 @@ import os
 import argparse
 from bs4 import BeautifulSoup
 import jinja2
+import simplejson as json
+import string
+
+
+def get_dblp_key(authors, year):
+
+    if isinstance(authors, str):
+        authors = [authors]
+
+    # start with first author's full name
+    key = authors[0].split()[-1]
+
+    # append co-authors' first letter from name
+    for au in authors[1:]:
+        for n in au.split()[::-1]:
+            if '.' in n:
+                continue
+            else:
+                key += n[:1]
+                break
+
+    # add year
+    key += str(year)[2:]
+
+    return ''.join([s for s in key if s in string.ascii_letters + string.digits + '-'])
+
+
+def check_key(key, keys):
+    if cur_json_key in json_output.keys():
+        print('double key {}'.format(cur_json_key))
+        return True
+    else:
+        return
 
 
 def parse_input(html):
@@ -44,3 +77,43 @@ if __name__ == '__main__':
     with open('output.txt', 'w') as f:
         html = template.render({'entries': entries})
         f.write(html)
+
+    # create json output
+    json_output = dict()
+    CONF_YEAR = 2018
+
+    for cur_entry in entries:
+
+        cur_authors = cur_entry['authors'].split(' and ')
+        cur_dblp_key = get_dblp_key(cur_authors, CONF_YEAR)
+        cur_json_key = 'conf/ismir/' + cur_dblp_key
+
+        cur_entry_json = dict()
+        cur_entry_json['@key'] = cur_json_key
+        cur_entry_json['@mdate'] = ''
+        cur_entry_json['author'] = cur_authors
+        cur_entry_json['title'] = cur_entry['title']
+        cur_entry_json['year'] = CONF_YEAR
+        cur_entry_json['crossref'] = 'conf/ismir/' + str(CONF_YEAR)
+        cur_entry_json['booktitle'] = 'ISMIR'
+        cur_entry_json['url'] = ''
+        cur_entry_json['ee'] = 'http://ismir2018.ircam.fr/doc/' + cur_entry['url'].replace('articles', 'pdfs')
+        cur_entry_json['record_id'] = ''
+        cur_entry_json['doi'] = ''
+
+        # check if key is unique
+        cur_key = cur_json_key
+        cur_char_idx = 0
+
+        while (cur_key in json_output.keys()):
+            # add small character to key to make unique
+            cur_key = cur_json_key + string.ascii_lowercase[cur_char_idx]
+
+        # just double-checking key uniqueness
+        if cur_key in json_output.keys():
+            raise('double key {}'.format(cur_json_key))
+
+        json_output[cur_key] = cur_entry_json
+
+    with open('output.json', 'w', encoding='utf-8') as f:
+        f.write(json.dumps(json_output, indent=2))
